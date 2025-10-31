9378028d0f89e905910d43b031b3a48746171f76
Your <you@example.com>
2025-10-31 16:43:09 -0500

    feat: Update API_BASE_URL to custom domain

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://pokerleaguebackend.onrender.com';
+export const API_BASE_URL = 'https://api.homepokerleague.com';
 //export const API_BASE_URL = 'http://192.168.0.26:8080';
